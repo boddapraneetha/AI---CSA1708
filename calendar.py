fbd2fd4d22d01e7e48eef3b812cb49fb5e3283e0
boddapraneetha <132537352+boddapraneetha@users.noreply.github.com>
2025-08-06 09:55:13 +0530

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,10 @@
+import calendar
+
+def show_calendar():
+    year = int(input("Enter year (e.g., 2025): "))
+    month = int(input("Enter month (1-12): "))
+    
+    print("\nHere is the calendar:\n")
+    print(calendar.month(year, month))
+
+show_calendar()
