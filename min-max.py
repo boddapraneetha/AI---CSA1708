7ef3ace8560086c1e0ea58590fd1ca6d05a7468c
boddapraneetha <132537352+boddapraneetha@users.noreply.github.com>
2025-08-06 09:56:50 +0530

    Add files via upload

---
+++ min-max.py
@@ -0,0 +1,8 @@
+numbers = [10, 5, 23, 89, 1, 56, 34]
+
+minimum = min(numbers)
+maximum = max(numbers)
+
+print("List of numbers:", numbers)
+print("Minimum number:", minimum)
+print("Maximum number:", maximum)
